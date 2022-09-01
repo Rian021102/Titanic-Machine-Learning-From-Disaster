95480c567716d3f35dd47ea70a7fbc6c97e36205
Rian Rachmanto <108880724+Rian021102@users.noreply.github.com>
2022-09-01 16:46:45 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+# read our pickle file and label our logisticmodel as model
+model = pickle.load(open('model_classifier.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    
+    if prediction==0:
+        return render_template('index.html',
+                               prediction_text='Survived'.format(prediction),
+                               )
+    else:
+        return render_template('index.html',
+                               prediction_text='Not Survived'.format(prediction),
+                              )
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
