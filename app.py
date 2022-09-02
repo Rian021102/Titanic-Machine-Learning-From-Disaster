06de79899d03365690bbff93eb91509f02b21d94
Rian Rachmanto <108880724+Rian021102@users.noreply.github.com>
2022-09-02 14:37:37 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,33 +1,31 @@
 import numpy as np
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-# read our pickle file and label our logisticmodel as model
-model = pickle.load(open('model_classifier.pkl', 'rb'))
 
-@app.route('/')
+model = pickle.load(open('model_classifier.pkl','rb'))
+
+@app.route('/',methods=["GET","POST"])
 def home():
     return render_template('Index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
 
-    int_features = [float(x) for x in request.form.values()]
+    int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    
+    output = round(prediction[0], 2)
+
     if prediction==0:
-        return render_template('Index.html',
-                               prediction_text='Survived'.format(prediction[0]),
-                               )
+            return render_template('Index.html',
+                                  prediction_text='Survived'.format(prediction),
+                                  )
     else:
-        return render_template('Index.html',
-                               prediction_text='Not Survived'.format(prediction[0]),
-                              )
-
-
-
+            return render_template('Index.html',
+                                  prediction_text='Not Survived'.format(prediction),
+                                  )
 if __name__ == "__main__":
     app.run(debug=True)
